8ae7e3258e345aa816e887a6fe444c247176756f
ebatemarco <ebatemarco@opc-l-wrk-0001.itbadc.itba.edu.ar>
2021-03-14 12:03:57 -0300

    Hay un Warning y falta mov robot

--- main.c
+++ main.c
@@ -13,39 +13,46 @@
 #include "ParseCmLine.h"
 #include "Simulation.h"
 
+int parseCallback(char *key, char *value, void *userData);
+
 int main(int argc, char** argv) 
 {
     Data_t userData = {0,0,0,0};
+    
+    parseCmdline(argc, argv, &parseCallback, &userData); 
+    
+    
     Simulation* Simulation1 = CreateSimulation(5, 5, 1); 
     Simulator(Simulation1);
     DeleteSimulation(Simulation1);
-    
-    int parseCallback(char *key, char *value, void *userData){
-    Data_t *Data= userData;
-    static int i=0;
-        if (strcmp(key,"height") == 0)
-        {
-            (Data->h)=value;
-        }
-        else if (strcmp(key,"width") == 0)
-        {
-            (Data->w)=value;
-        }
-        else if (strcmp(key,"robots") == 0)
-        {
-            (Data->robots)=value;
-        }
-        else if (strcmp(key,"mode") == 0)
-        {
-            (Data->mode)=value;
-        }
-        else {
-            printf("Error, solo se aceptan los parametros de simulacion\n");
-            return ERROR;
-        }
-    }
 
     return (EXIT_SUCCESS);
+}
+
+int parseCallback(char *key, char *value, void *userData)
+{
+    Data_t *Data= userData;
+    static int i=0;
+    if (strcmp(key,"height") == 0)
+    {
+        (Data->h)=value;
+    }
+    else if (strcmp(key,"width") == 0)
+    {
+        (Data->w)=value;
+    }
+    else if (strcmp(key,"robots") == 0)
+    {
+        (Data->robots)=value;
+    }
+    else if (strcmp(key,"mode") == 0)
+    {
+        (Data->mode)=value;
+    }
+    else {
+        printf("Error, solo se aceptan los parametros de simulacion\n");
+        return ERROR;
+    }
 }
 
 
