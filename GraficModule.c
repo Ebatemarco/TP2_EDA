7cf6a0da20b1a32f58e70332c964abe8b5e3e840
ebatemarco <ebatemarco@opc-l-wrk-0004.itbadc.itba.edu.ar>
2021-03-15 13:37:59 -0300

    Sacamos Allegro para resolver problemas de n robots

--- GraficModule.c
+++ GraficModule.c
@@ -17,7 +17,7 @@ void PrintFloor(Floor* F, Robot* R, long NoRobots)//Por ahora con solo un robot
         printf("\n");
         for(long j=0; j< (F->Width); j++)
         {
-            for(int k=0;k< (sizeof(Robot)*NoRobots);k+=sizeof(Robot))//Revisamos si en la casilla hay algun robot de entre los n que tenemos
+            for(int k=0;k< (NoRobots);k++)//Revisamos si en la casilla hay algun robot de entre los n que tenemos
             {
                 if( (i == floor(((R+k)->y))) && (j == floor(((R+k)->x))) )
                 {
@@ -44,75 +44,4 @@ void PrintFloor(Floor* F, Robot* R, long NoRobots)//Por ahora con solo un robot
     }
     printf("\n");
     
-}
-
-
-int AllegroSupport(void)
-{
-    must_init(al_init(), "allegro");
-
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    must_init(timer, "timer");
-
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    must_init(queue, "queue");
-
-    ALLEGRO_DISPLAY* disp = al_create_display(H*5, W*5);
-    must_init(disp, "display");
-
-    ALLEGRO_FONT* font = al_create_builtin_font();
-    must_init(font, "font");
-    
-    must_init(al_init_primitives_addon(), "primitives");
-
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
-    
-    return 0;
-}
-
-int AllegroFloor(Floor* F, Robot* R, long NoRobots)
-{   
-    //ALLEGRO_EVENT event;
-    
-    
-    bool HereIsARobot=1; 
-    for(long i= 0; i< (F->Height) ; i++ )
-    {
-        printf("\n");
-        for(long j=0; j< (F->Width); j++)
-        {
-            for(int k=0;k< (sizeof(Robot)*NoRobots);k+=sizeof(Robot))//Revisamos si en la casilla hay algun robot de entre los n que tenemos
-            {
-                if( (i == floor(((R+k)->y))) && (j == floor(((R+k)->x))) )
-                {
-                    al_draw_filled_rectangle(j, i, j+(W/(F->Width)), i+(W/(F->Height)), al_map_rgba_f(0, 0, 1, 1));
-                    HereIsARobot=0;
-                    break; //Si un robot esta en la casilla no nos importa si en la misma hay mas solo la pintamos una vez
-                }
-            }
-            if(HereIsARobot)//Si en la casilla actual no hay un robot Pinto acorde
-            {
-                if( ((F->Tiles)[i*(F->Width)+j]) == SUCIO) 
-                {
-                    al_draw_filled_rectangle(j, i, j+(W/(F->Width)), i+(W/(F->Height)), al_map_rgba_f(0, 0, 0, 0));
-                }
-                else if(((F->Tiles)[i*(F->Width)+j]) == LIMPIO) 
-                {
-                    al_draw_filled_rectangle(j, i, j+(W/(F->Width)), i+(W/(F->Height)), al_map_rgba_f(1, 0, 0, 1));
-                }
-            }
-            HereIsARobot=1;
-                
-        }
-        
-    }
-}
-
-void must_init(bool test, const char *description)
-{
-    if(test) return;
-
-    printf("couldn't initialize %s\n", description);
-    exit(1);
 }
--- Simulation.c
+++ Simulation.c
@@ -59,7 +59,7 @@ bool AllClear(Floor* F) //Si el piso esta limpio da 1
 
 void SimulationUpdate(Robot* R, Floor* F, long NoRobots)
 {
-    for(int i=0;i< (sizeof(Robot)*NoRobots);i+=sizeof(Robot))
+    for(int i=0; i < (NoRobots);i++)
     {
         srand(time(0));
         int Flag = 0;//Creamos el flag para los cuadrantes
@@ -84,7 +84,7 @@ void SimulationUpdate(Robot* R, Floor* F, long NoRobots)
 
 void ClearPoint(Floor* F, Robot* R, long NoRobots)
 {
-    for(int i=0;i< (sizeof(Robot)*NoRobots);i+=sizeof(Robot))
+    for(int i=0;i< NoRobots; i++)
     {
         *( (F->Tiles) + sizeof(bool)* ((int)((R+i)->x)) + sizeof(bool)*((int)((R+i)->y)) * (F->Width)) = LIMPIO;
     }
@@ -92,66 +92,12 @@ void ClearPoint(Floor* F, Robot* R, long NoRobots)
 
 void Simulator(Simulation* Simu)
 {
-    //AllegroSupport();
-    /*
-    must_init(al_init(), "allegro");
-
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    must_init(timer, "timer");
-
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    must_init(queue, "queue");
-
-    ALLEGRO_DISPLAY* disp = al_create_display(H*5, W*5);
-    must_init(disp, "display");
-
-    ALLEGRO_FONT* font = al_create_builtin_font();
-    must_init(font, "font");
-    
-    must_init(al_init_primitives_addon(), "primitives");
-
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));*/
     
     while( !(AllClear(Simu->f) ))
     {
         SimulationUpdate(Simu->robs, Simu->f, Simu->NoRobots);//Vemos la nueva pocicion del robot
         ClearPoint(Simu->f, Simu->robs, Simu->NoRobots);
         PrintFloor(Simu->f, Simu->robs, Simu->NoRobots);//Dibuajmos el piso
-        
-        //AllegroFloor(Simu->f, Simu->robs, Simu->NoRobots, );
-        /*
-        bool HereIsARobot=1; 
-        for(long i= 0; i< (Simu->f->Height) ; i++ )
-        {
-            
-            for(long j=0; j< (Simu->f->Width); j++)
-            {
-                for(int k=0;k< (sizeof(Robot)*Simu->NoRobots);k+=sizeof(Robot))//Revisamos si en la casilla hay algun robot de entre los n que tenemos
-                {
-                    if( (i == floor(((Simu->robs+k)->y))) && (j == floor(((Simu->robs+k)->x))) )
-                    {
-                        al_draw_filled_rectangle(j, i, j+(W/(Simu->f->Width)), i+(W/(Simu->f->Height)), al_map_rgba_f(0, 0, 1, 1));
-                        HereIsARobot=0;
-                        break; //Si un robot esta en la casilla no nos importa si en la misma hay mas solo la pintamos una vez
-                    }
-                }
-                if(HereIsARobot)//Si en la casilla actual no hay un robot Pinto acorde
-                {
-                    if( ((Simu->f->Tiles)[i*(Simu->f->Width)+j]) == SUCIO) 
-                    {
-                        al_draw_filled_rectangle(j, i, j+(W/(Simu->f->Width)), i+(W/(Simu->f->Height)), al_map_rgba_f(0, 0, 0, 0));
-                    }
-                    else if(((Simu->f->Tiles)[i*(Simu->f->Width)+j]) == LIMPIO) 
-                    {
-                        al_draw_filled_rectangle(j, i, j+(W/(Simu->f->Width)), i+(W/(Simu->f->Height)), al_map_rgba_f(1, 0, 0, 1));
-                    }
-                }
-                HereIsARobot=1;
-
-            }
-
-        }*/
         sleep(TIME);
         
     }//Mientras el piso este sucio segumos simulando
