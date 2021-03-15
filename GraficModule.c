60a43c17fd0b1becb9fa9616ad88776717542aa2
ebatemarco <ebatemarco@opc-l-wrk-0004.itbadc.itba.edu.ar>
2021-03-15 14:05:29 -0300

    Comentamos PrintFloor

--- GraficModule.c
+++ GraficModule.c
@@ -9,35 +9,35 @@
 
 
 
-void PrintFloor(Floor* F, Robot* R, long NoRobots)//Por ahora con solo un robot
+void PrintFloor(Floor* F, Robot* R, long NoRobots)
 {
-    bool HereIsARobot=1; 
-    for(long i= 0; i< (F->Height) ; i++ )
+    bool HereIsARobot=0; //Flag que nos sirve para pintar los robots en un mismo bloque con printf
+    for(long i= 0; i< (F->Height) ; i++ )//Vamos fila a fila
     {
         printf("\n");
-        for(long j=0; j< (F->Width); j++)
+        for(long j=0; j< (F->Width); j++)//Columna a columna
         {
             for(int k=0;k< (NoRobots);k++)//Revisamos si en la casilla hay algun robot de entre los n que tenemos
             {
                 if( (i == floor(((R+k)->y))) && (j == floor(((R+k)->x))) )
                 {
                     printf(ANSI_COLOR_RED  "* "  ANSI_COLOR_RESET);
-                    HereIsARobot=0;
+                    HereIsARobot=1;//Marcamos que en la casilla hay robot
                     break; //Si un robot esta en la casilla no nos importa si en la misma hay mas solo la pintamos una vez
                 }
             }
-            if(HereIsARobot)//Si en la casilla actual no hay un robot Pinto acorde
+            if(!HereIsARobot)//Si en la casilla actual no hay un robot Pinto acorde
             {
-                if( ((F->Tiles)[i*(F->Width)+j]) == SUCIO) 
+                if( ((F->Tiles)[i*(F->Width)+j]) == SUCIO) //Veo si esta sucia
                 {
                     printf(ANSI_COLOR_GREEN  "* "  ANSI_COLOR_RESET);
                 }
-                else if(((F->Tiles)[i*(F->Width)+j]) == LIMPIO) 
+                else if(((F->Tiles)[i*(F->Width)+j]) == LIMPIO) //veo si esta limpia 
                 {
                     printf(ANSI_COLOR_YELLOW  "* "  ANSI_COLOR_RESET);
                 }
             }
-            HereIsARobot=1;
+            HereIsARobot=0;//Reiniciamos el flag
                 
         }
         
